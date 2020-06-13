4277f9093a0cf7bf73541e9b9b504f8399653457
Daniel Williams <dancart2001@gmail.com>
2020-06-13 15:55:01 +0100

    removed static from .gitignore

--- blog/static/blog/base.js
+++ blog/static/blog/base.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    var instance = M.Tabs.init(document.querySelectorAll('.tabs'), {"onShow":load_page});
+    var instance = M.Tabs.init(document.querySelectorAll('.tabs'), { "onShow": load_page });
 
     document.querySelectorAll('.tab-link').forEach(element => {
-        element.onclick = function(){
+        element.onclick = function () {
             console.log(element.id)
             load_page(element.id)
         }
     });
-
-
     // Start by loading first page.
     load_page('blog');
+    
 
 });
 
 // Renders contents of new page in main view.
 function load_page(id) {
-    
+
     const request = new XMLHttpRequest();
     request.open('GET', `/${id}`);
     request.onload = () => {
@@ -27,42 +26,42 @@ function load_page(id) {
         // Push state to URL.
         document.title = id;
         //history.pushState({'title': id, 'text': response, 'tabID':id}, id, id);
-        if (id=="blog"){
-            document.querySelector('.content').innerHTML="";
+        if (id == "blog") {
+            document.querySelector('.content').innerHTML = "";
             const template = Handlebars.compile(document.querySelector('#blog_post').innerHTML);
-            for(i=0;i<data.posts.length;i++){
+            for (i = 0; i < data.posts.length; i++) {
                 const post = data.posts[i];
-                const content = template({"title":post.title, "author": "author", "date":"date", "content":post.content});
+                const content = template({ "title": post.title, "author": "author", "date": "date", "content": post.content });
                 document.querySelector('.content').innerHTML += content;
             }
-            document.querySelectorAll('.post').forEach( function(element){
+            document.querySelectorAll('.post').forEach(function (element) {
                 element.classList.remove("hideit");
                 element.classList.add("showit");
             })
         }
-        else if (id=="portfolio"){
-            document.querySelectorAll('.post').forEach( function(element){
+        else if (id == "portfolio") {
+            document.querySelectorAll('.post').forEach(function (element) {
                 //element.style.animationPlayState = 'running';
                 element.classList.remove("showit");
                 element.classList.add("hideit");
-                element.addEventListener('animationend', () =>  {
-                    if (element.classList.contains("hideit")){
-                        document.querySelector('.content').innerHTML="";
+                element.addEventListener('animationend', () => {
+                    if (element.classList.contains("hideit")) {
+                        document.querySelector('.content').innerHTML = "";
                     }
                 });
             })
         }
     };
     request.send();
-    
+
 }
 
-  // Update text on popping state.
-  window.onpopstate = e => {
+// Update text on popping state.
+window.onpopstate = e => {
     const data = e.state;
     document.title = data.title;
     var instance = M.Tabs.getInstance(document.querySelectorAll('.tabs'));
-    instance.select('#'+data.tabID)
+    instance.select('#' + data.tabID)
     //document.querySelector('#'+data.tabID).select()
     document.querySelector('.content').innerHTML = data.text;
 };
